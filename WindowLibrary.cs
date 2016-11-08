a5e009ab48aa64a02ef3924b5f44641ecab77f2d
minhthe <minhthe.007@gmail.com>
2016-11-08 10:23:52 +0700

    Step 6 :- PIC Pattern for decoupling ( Simple Factory Pattern)
    Acronym of PIC is “Polymorphism + Interfaces + Centralizing object creation”

--- WindowLibrary.cs
+++ WindowLibrary.cs
@@ -18,21 +18,15 @@ namespace DesignPatternStepByStep
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
-        {
-            CustomerBase custbase = null;
-            if (cmbCustomerType.SelectedIndex == 0)
-            {
-                custbase = new Lead();
-            }
-            else
-            {
-                custbase = new Customer();
-            }
+{
+            ICustomerInterface custbase = null;
+            FactoryCustomer obj = new FactoryCustomer();
+            custbase = obj.Create(cmbCustomerType.SelectedIndex);
             custbase.CustomerName = txtCustomerName.Text;
             custbase.Address = txtAddress.Text;
             custbase.PhoneNumber = txtPhoneNumber.Text;
             custbase.BillDate = Convert.ToDateTime(txtBillingDate.Text);
             custbase.BillAmount = Convert.ToDecimal(txtBillingAmount.Text);
-        }
+}
     }
 }
