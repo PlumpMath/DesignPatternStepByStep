d9f8eae9e3a78a7be6c1cbaa5053ff4475dfb6d2
minhthe <minhthe.007@gmail.com>
2016-11-08 08:34:21 +0700

    Step 1 : Indentify your entites / objects Have 2 entites : Lead and Customer Signed-off-by: minhthe <minhthe.007@gmail.com>

---
+++ CustomerLibrary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternStepByStep
+{
+    public class Lead
+    {
+        public string LeadName { get; set; }
+
+        public string PhoneNumber{ get; set; }
+
+        public string BillAmount { get; set; }
+
+        public string BillDate{ get; set; }
+
+        public string Address { get; set; }
+
+    }
+    public class Custommer
+    {
+        public string CustomerName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string BillAmount { get; set; }
+
+        public string BillDate { get; set; }
+
+        public string Address { get; set; }
+
+    }
+}
