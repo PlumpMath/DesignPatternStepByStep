569e15a2acf71c9a7a69f316426d8f9503c0f1f0
minhthe <minhthe.007@gmail.com>
2016-11-08 08:47:43 +0700

    Step 2 : Indentify relationships between entites: - entities have to interact with each other, so they have relationships. - Son “IS A“ child of his father and son “HAS A” a car gifted by his father. - “IS A” is more of a parent child relationship (hierarchical) - while “HAS A” is more of a using relationship(Aggregated , Composed and Associated). - Less is_A type of customer with less validations. - Is A” relationship is a parent child relationship while “HAS A” is a using relationship.

--- CustomerLibrary.cs
+++ CustomerLibrary.cs
@@ -6,30 +6,52 @@ using System.Threading.Tasks;
 
 namespace DesignPatternStepByStep
 {
-    public class Lead
-    {
-        public string LeadName { get; set; }
-
-        public string PhoneNumber{ get; set; }
-
-        public string BillAmount { get; set; }
-
-        public string BillDate{ get; set; }
-
-        public string Address { get; set; }
-
-    }
     public class Custommer
     {
         public string CustomerName { get; set; }
-
         public string PhoneNumber { get; set; }
-
-        public string BillAmount { get; set; }
-
-        public string BillDate { get; set; }
-
+        public decimal BillAmount { get; set; }
+        public DateTime BillDate { get; set; }
         public string Address { get; set; }
 
+        //the new classes can override the validate logic
+        public virtual void Validate()
+        {
+            if (CustomerName.Length == 0)
+            {
+                throw new Exception("Customer Name is required");
+            }
+
+            if (PhoneNumber.Length == 0)
+            {
+                throw new Exception("Phone Number is required");
+            }
+
+            if (BillAmount < 0)
+            {
+                throw new Exception("Bill is required");
+            }
+
+            if (BillDate > DateTime.Now)
+            {
+                throw new Exception("Bill Date is not proper");
+            }
+        }
+    }
+
+    public class Lead : Custommer
+    {
+        public override void Validate()
+        {
+            if (CustomerName.Length == 0)
+            {
+                throw new Exception("Customer Name is required");
+            }
+
+            if (PhoneNumber.Length == 0)
+            {
+                throw new Exception("Phone Number is required");
+            }
+        }
     }
 }
