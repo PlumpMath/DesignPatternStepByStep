e5d1c0deacb1b85b7fe094aa883b0d7e982e6901
minhthe <minhthe.007@gmail.com>
2016-11-09 09:46:09 +0700

    Step 11:- Automating Simple Factory using Unity -if we need to support other object types like order , logger and so on. So writing factory for each one of them , creating polymorphic collection , -look ups and then on top of it testing all these things is a huge task by itself -The complete simple factory and the polymorphic collection look up can be automated / replaced by using some DI frameworks like unity , ninject , MEF etc. -In unity or any DI framework we have concept of the containers. These containers are nothing but collections. “RegisterType” -and “ResolveType” methods helps to add and get objects from the container collection respectively.

--- FactoryCustomer.cs
+++ FactoryCustomer.cs
@@ -3,28 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
 
 namespace DesignPatternStepByStep
 {
     public static class FactoryCustomer
     {
-        private static Lazy<List<ICustomerInterface>> customers = null;
+        private static IUnityContainer cont = null;
 
         public FactoryCustomer()
         {
-            customers = new Lazy<List<ICustomerInterface>>(() => LoadCustomers() );
-        }
-        private List<ICustomerInterface> LoadCustomers()
-        {
-            List<ICustomerInterface> customers = new List<ICustomerInterface>();
-            customers.Add(new Lead());
-            customers.Add(new Customer());
-            return customers
+            cont = new UnityContainer();
+            cont.RegisterType<ICustomerInterface, Lead="">("0");
+            cont.RegisterType<ICustomerInterface, Customer="" > ("1");
+
         }
 
         public static ICustomerInterface Create(int CustomerType)
         {
-            return customers.Value[CustomerType].Clone();
+            return cont.Resolve<ICustomerInterface>(CustomerType.ToString());
         }
     }
 }
